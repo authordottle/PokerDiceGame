1896359197d9a06c1642330b01d807cda702b324
Jachian <author.tle@gmail.com>
2023-10-26 14:52:22 -0400

    do code refinement

--- src/main/java/org/cse1223/Main.java
+++ src/main/java/org/cse1223/Main.java
@@ -1,45 +1,33 @@
 package org.cse1223;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        boolean decide;
         int[] dice = new int[5];
-        resetDice(dice);
-        generateCurrentDice(dice);
-
-        System.out.println("Your current dice: " + diceToString(dice));
-
-        System.out.print("Select a die to re-roll (-1 to keep remaining dice): ");
-        Scanner keyboard = new Scanner(System.in);
-        promptForReroll(dice, keyboard);
-
-        rollDice(dice);
-        System.out.print("\n");
-        System.out.println(getResult(dice));
-        System.out.print("\n");
-
-        boolean decide = promptForPlayAgain(keyboard);
-
-        while (decide) {
-            dice = new int[5];
+        do {
             resetDice(dice);
             generateCurrentDice(dice);
 
             System.out.println("Your current dice: " + diceToString(dice));
+
             System.out.print("Select a die to re-roll (-1 to keep remaining dice): ");
+            Scanner keyboard = new Scanner(System.in);
             promptForReroll(dice, keyboard);
 
             rollDice(dice);
             System.out.print("\n");
-
             System.out.println(getResult(dice));
-            decide = promptForPlayAgain(keyboard);
-        }
+            System.out.print("\n");
 
-        System.out.print("Goodbye.");
+            decide = promptForPlayAgain(keyboard);
+        } while (decide);
+
+        System.out.println("Goodbye.");
     }
 
     // Given an array of integers as input, sets every element of the array to zero.
@@ -49,6 +37,7 @@ public class Main {
         }
     }
 
+    // Generate random dice
     private static void generateCurrentDice(int[] dice) {
         for (int i = 0; i < 5; i++) {
             int roll = (int) (Math.random() * 6) + 1;
@@ -61,7 +50,7 @@ public class Main {
     // it. Otherwise, leave it as is and move to the next element.
     private static void rollDice(int[] dice) {
         System.out.println("Keeping remaining dice...");
-        System.out.println("Rerolling...");
+        System.out.println("Re-rolling...");
 
         for (int i = 0; i < 5; i++) {
             if (dice[i] == 0) {
@@ -78,11 +67,11 @@ public class Main {
     // array contains the values [0, 3, 6, 5, 2] then the String returned by this method
     // should be "0 3 6 5 2".
     private static String diceToString(int[] dice) {
-        String dicestr = "";
+        StringBuilder dicestr = new StringBuilder();
         for (int i = 0; i < 5; i++) {
-            dicestr = dicestr + dice[i] + " ";
+            dicestr.append(dice[i]).append(" ");
         }
-        return dicestr;
+        return dicestr.toString();
     }
 
     // Given an array of integers and a Scanner as input, prompt the user with a message
@@ -121,22 +110,19 @@ public class Main {
             System.out.print("Would you like to play again [Y/N]?:");
             input = inScanner.nextLine();
         }
-        if (input.equals("Y")) {
-            return true;
-        }
-        return false;
+        return input.equals("Y");
     }
 
     // Given an array of integers, determines the result as a hand of Poker Dice.  The
     // result is determined as:
-    //	* Five of a kind - all five integers in the array have the same value
-    //	* Four of a kind - four of the five integers in the array have the same value
-    //	* Full House - three integers in the array have the same value, and the remaining two
+    //	* Five of a kind - all 5 integers in the array have the same value
+    //	* Four of a kind - 4 of the five integers in the array have the same value
+    //	* Full House - 3 integers in the array have the same value, and the remaining 2
     //					integers have the same value as well (Three of a kind and a pair)
-    //	* Three of a kind - three integers in the array have the same value
-    //	* Two pair - two integers in the array have the same value, and two other integers
+    //	* Three of a kind - 3 integers in the array have the same value
+    //	* Two pair - 2 integers in the array have the same value, and 2 other integers
     //					in the array have the same value
-    //	* One pair - two integers in the array have the same value
+    //	* One pair - 2 integers in the array have the same value
     //	* Highest value - if none of the above hold true, the Highest Value in the array
     //						is used to determine the result
     //
@@ -153,49 +139,40 @@ public class Main {
             if (greatestnum < dice[pos + 1]) {
                 greatestnum = dice[pos + 1];
             }
-            sum = sum + dice[pos + 1]; //Extra credit for Straight
-            pos++;
-
-        }
-
-        int[] newdice = getCounts(dice);
-        pos = 0;
-        int largest = newdice[pos];
-        while (pos < 4) {
-            if (largest < newdice[pos + 1]) {
-                largest = newdice[pos + 1];
-            }
+            sum += dice[pos + 1]; // Extra credit for Straight
             pos++;
         }
 
-        int count = 0;
+        int[] eachDiceNumberCountArray = getCounts(dice);
+        int largestDiceNumberCount = Arrays.stream(eachDiceNumberCountArray).max().getAsInt();
+
+        int numberTwoCount = 0;
         pos = 0;
         while (pos < 5) {
-            if (newdice[pos] == 2) {
-                count++;
+            if (eachDiceNumberCountArray[pos] == 2) {
+                numberTwoCount++;
             }
             pos++;
-
         }
 
         String output;
-        if (largest == 5) {
+        if (largestDiceNumberCount == 5) {
             output = "Five of a Kind";
-        } else if (largest == 4) {
+        } else if (largestDiceNumberCount == 4) {
             output = "Four of a Kind";
-        } else if (largest == 3) {
-            if (count == 2) {
+        } else if (largestDiceNumberCount == 3) {
+            if (numberTwoCount == 2) {
                 output = "Full House ";
             } else {
                 output = "Three of a Kind";
             }
-        } else if (largest == 2) {
-            if (count == 2) {
+        } else if (largestDiceNumberCount == 2) {
+            if (numberTwoCount == 2) {
                 output = "Two pair";
             } else {
                 output = "One pair";
             }
-        } else if ((sum / 5 == 4 && sum % 5 == 0) || (sum / 5 == 3 && sum % 5 == 0)) {//Extra credit for Straight
+        } else if ((sum / 5 == 4 && sum % 5 == 0) || (sum / 5 == 3 && sum % 5 == 0)) { // Extra credit for Straight
             output = "Straight";
         } else {
             output = "Highest value " + greatestnum;
@@ -205,14 +182,13 @@ public class Main {
     }
 
     // Given an array of integers as input, return back an array with the counts of the
-    // individual values in it.  You may assume that all elements in the array will have
-    // a value between 1 and 6.  For example, if the array passed into the method were:
-    //   [1, 2, 3, 3, 5]
-    // Then the array of counts returned back by this method would be:
-    // [1, 1, 2, 0, 1, 0]
+    // individual values in it. You may assume that all elements in the array will have
+    // a value between 1 and 6.
+    // For example, if the array passed into the method were: [1, 2, 3, 3, 5]
+    // Then the array of counts returned back by this method would be: [1, 1, 2, 0, 1, 0]
     // (Where index 0 holds the counts of the value 1, index 1 holds the counts of the value
     //  2, index 2 holds the counts of the value 3, etc.)
-    // HINT:  This method is very useful for determining the score of a particular hand
+    // HINT: This method is very useful for determining the score of a particular hand
     //  of poker dice.  Use it as a helper method for the getResult() method above.
     private static int[] getCounts(int[] dice) {
         int check = 0;
